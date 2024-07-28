f2b0e6ade0329cc124eb3963b16f41a37272a508
Piotr Waclawczyk <piotr@piotrwaclawczyk.pl>
2024-07-28 11:35:41 +0200

    Add project files.

---
+++ WPFGridRowNColumnFix/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WPFGridRowNColumnFix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGMinus = new System.Windows.Forms.Button();
+            this.btnGPlus = new System.Windows.Forms.Button();
+            this.btnCMinus = new System.Windows.Forms.Button();
+            this.btnCPlus = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1077, 648);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnGMinus
+            // 
+            this.btnGMinus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGMinus.Location = new System.Drawing.Point(13, 668);
+            this.btnGMinus.Name = "btnGMinus";
+            this.btnGMinus.Size = new System.Drawing.Size(174, 63);
+            this.btnGMinus.TabIndex = 1;
+            this.btnGMinus.Text = "Grid.Row -= 1";
+            this.btnGMinus.UseVisualStyleBackColor = true;
+            this.btnGMinus.Click += new System.EventHandler(this.btnGMinus_Click);
+            // 
+            // btnGPlus
+            // 
+            this.btnGPlus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGPlus.Location = new System.Drawing.Point(193, 668);
+            this.btnGPlus.Name = "btnGPlus";
+            this.btnGPlus.Size = new System.Drawing.Size(174, 63);
+            this.btnGPlus.TabIndex = 2;
+            this.btnGPlus.Text = "Grid.Row += 1";
+            this.btnGPlus.UseVisualStyleBackColor = true;
+            this.btnGPlus.Click += new System.EventHandler(this.btnGPlus_Click);
+            // 
+            // btnCMinus
+            // 
+            this.btnCMinus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCMinus.Location = new System.Drawing.Point(373, 666);
+            this.btnCMinus.Name = "btnCMinus";
+            this.btnCMinus.Size = new System.Drawing.Size(174, 63);
+            this.btnCMinus.TabIndex = 3;
+            this.btnCMinus.Text = "Grid.Column -= 1";
+            this.btnCMinus.UseVisualStyleBackColor = true;
+            this.btnCMinus.Click += new System.EventHandler(this.btnCMinus_Click);
+            // 
+            // btnCPlus
+            // 
+            this.btnCPlus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCPlus.Location = new System.Drawing.Point(553, 666);
+            this.btnCPlus.Name = "btnCPlus";
+            this.btnCPlus.Size = new System.Drawing.Size(174, 63);
+            this.btnCPlus.TabIndex = 4;
+            this.btnCPlus.Text = "Grid.Column += 1";
+            this.btnCPlus.UseVisualStyleBackColor = true;
+            this.btnCPlus.Click += new System.EventHandler(this.btnCPlus_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPaste.Location = new System.Drawing.Point(733, 666);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(174, 63);
+            this.btnPaste.TabIndex = 5;
+            this.btnPaste.Text = "Paste";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCopy.Location = new System.Drawing.Point(913, 666);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(174, 63);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 742);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCPlus);
+            this.Controls.Add(this.btnCMinus);
+            this.Controls.Add(this.btnGPlus);
+            this.Controls.Add(this.btnGMinus);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Grid Fix";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGMinus;
+        private System.Windows.Forms.Button btnGPlus;
+        private System.Windows.Forms.Button btnCMinus;
+        private System.Windows.Forms.Button btnCPlus;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ WPFGridRowNColumnFix/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WPFGridRowNColumnFix
+{
+    public partial class Form1 : Form
+    {
+        const int N = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        private void btnGMinus_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            for (int i = 0; i <= N; i++)
+            {
+                s = s.Replace("Grid.Row=\"" + i + "\"", "Grid.Row=\"" + (i - 1) + "\"");
+            }
+            textBox1.Text = s;
+        }
+
+        private void btnGPlus_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            for (int i = N; i >= 0; i--)
+            {
+                s = s.Replace("Grid.Row=\"" + i + "\"", "Grid.Row=\"" + (i + 1) + "\"");
+            }
+            textBox1.Text = s;
+        }
+
+        private void btnCMinus_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            for (int i = 0; i <= N; i++)
+            {
+                s = s.Replace("Grid.Column=\"" + i + "\"", "Grid.Column=\"" + (i - 1) + "\"");
+            }
+            textBox1.Text = s;
+        }
+
+        private void btnCPlus_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            for (int i = N; i >= 0; i--)
+            {
+                s = s.Replace("Grid.Column=\"" + i + "\"", "Grid.Column=\"" + (i + 1) + "\"");
+            }
+            textBox1.Text = s;
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Clipboard.GetText();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Clipboard.GetText();
+        }
+    }
+}
